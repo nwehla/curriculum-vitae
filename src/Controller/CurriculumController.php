6af2fac86847f9bcfaca2a563730278a02121956
nwehla <nwehlabatoum@laposte.net>
2021-10-05 06:48:16 +0200

    mon premier commit cv

---
+++ src/Controller/CurriculumController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CurriculumController extends AbstractController
+{
+    /**
+     * @Route("/curriculum", name="curriculum")
+     */
+    public function index(): Response
+    {
+        return $this->render('curriculum/index.html.twig', [
+            'controller_name' => 'CurriculumController',
+        ]);
+    }
+}
